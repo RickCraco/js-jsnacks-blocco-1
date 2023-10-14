15bb3e2886e3b0a60ae4e5ae3bd0713b60917584
RickCraco <riccardocracolici43@gmail.com>
2023-10-14 12:19:16 +0200

    aggiunto algoritmo risoluzione primo snack

---
+++ array-lista/script.js
@@ -0,0 +1,35 @@
+//esercizio frutta
+
+const frutta = ['banana', 'mela', 'pera', 'ciliegia', 'arancia', 'mandarino', 'cocomero', 'limone', 'fragola'];
+
+frutta.push('pesca');
+
+
+let cocomeroTrovato = false;
+for (let i = 0; i < frutta.length; i++) {
+    if (frutta[i] === 'cocomero') {
+        cocomeroTrovato = true;
+        frutta.splice(i, 1);
+        break;
+    }
+}
+
+if (cocomeroTrovato) {
+    console.log('Trovato: cocomero era presente nell\'array e ora è stato rimosso.');
+} else {
+    console.log('Non trovato: cocomero non era presente nell\'array.');
+}
+
+cocomeroTrovato = false;
+for (let i = 0; i < frutta.length; i++) {
+    if (frutta[i] === 'cocomero') {
+        cocomeroTrovato = true;
+        break;
+    }
+}
+
+if (cocomeroTrovato) {
+    console.log('Trovato: cocomero è presente nell\'array modificato.');
+} else {
+    console.log('Non trovato: cocomero non è presente nell\'array modificato.');
+}
