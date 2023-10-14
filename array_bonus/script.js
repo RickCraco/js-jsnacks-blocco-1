051458c338c670660ae51e9733b167fb6c82b0e9
RickCraco <riccardocracolici43@gmail.com>
2023-10-14 12:21:15 +0200

    aggiunta risoluzione algoritmo snack bonus

---
+++ array_bonus/script.js
@@ -0,0 +1,14 @@
+//esercizio bonus
+
+let vettore1 = [1, 2, 3, 4, 5];
+let vettore2 = [1, 2, 3];
+
+for(let i = 0; i < (vettore1.length + 1) - vettore2.length; i++){
+  vettore2.push(getRndInteger(1, 100));
+}
+
+if(vettore1.length === vettore2.length){
+  console.log('i due vettori hanno la stessa lunghezza');
+}else{
+  console.log('i due vettori hanno ancora lunghezze diverse');
+}
